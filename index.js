c5de58302f6da134aeca0be8fc1031522479032e
Mihir Rege <mihirrege@gmail.com>
2014-11-01 14:40:51 +0530

    get auth_code by directly calling browser

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+var restify = require('restify');
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 var config = require('./auth.json');
 var fs = require('fs');
-
+var spawn = require('child_process').spawn;
 var CLIENT_ID = config.installed.client_id;
 var CLIENT_SECRET = config.installed.client_secret;
-var REDIRECT_URL = "http://localhost";
+var port = 3645;
+var REDIRECT_URL = "http://localhost:" + port +"/callback";
 var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
 google.options({ auth: oauth2Client });
 
@@ -22,35 +24,67 @@ var scopes = [
 ];
 
 if(!config.auth_code){
+
+    var server = restify.createServer({
+        name: 'google-auth-callback',
+    });
+    server.use(restify.queryParser());
+
+    server.get('/callback', function(req, res, next){
+        config.auth_code = req.query.code;
+        fs.writeFile('./auth.json', JSON.stringify(config));
+        startWatching();
+        res.send("Authenticated :)");
+        next();
+    });
+    server.listen(port);
     var url = oauth2Client.generateAuthUrl({
         //access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
         scope: scopes // If you only need one scope you can pass it as string
     });
 
-    console.log("Generate access token from this url ", url);
-    console.log("Put it in config.json with key auth_code");
-    process.exit();
-}
+    var command;
+    if(process.platform === 'linux'){
+        command = 'xdg-open';
+    }
+    if(process.platform === 'darwin'){
+        commmand = 'open';
+    }
 
-
-var tokens;
-if (fs.existsSync('./tokens.json')) {
-    tokens = require('./tokens.json');
+    if(command){
+        spawn(command, [url]);
+    }
+    else{
+        console.log("Generate access token from this url \n", url);
+    }
 }
 else{
-    console.log("Using authcode", config.auth_code);
-    oauth2Client.getToken(config.auth_code, function(err, tks) {
-        // Now tokens contains an access_token and an optional refresh_token. Save them.
-        if(err) {
-            console.log(err);
-            return;
-        }
-        tokens =tks;
-        console.log(tokens);
-        fs.writeFile('./tokens.json', tokens);
-        oauth2Client.setCredentials(tokens);
-        setInterval(checkMail, 10000);
-    });
+    startWatching();
+}
+
+var tokens;
+
+function startWatching(){
+    if (fs.existsSync('./tokens.json')) {
+        tokens = require('./tokens.json');
+    }
+    else{
+        console.log("Using authcode", config.auth_code);
+        oauth2Client.getToken(config.auth_code, function(err, tks) {
+            // Now tokens contains an access_token and an optional refresh_token. Save them.
+            if(err) {
+                console.log(err);
+                return;
+            }
+            tokens =tks;
+            console.log(tokens);
+            fs.writeFile('./tokens.json', JSON.stringify(tokens));
+
+
+        });
+    }
+    oauth2Client.setCredentials(tokens);
+    setInterval(checkMail, 10000);
 }
 
 
