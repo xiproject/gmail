6cfd4d5f767c83ab8cc5e85685da1a132fda03fc
Mihir Rege <mihirrege@gmail.com>
2014-11-02 06:11:01 +0530

    Handle different MIME types

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var xal = require('../../xal-javascript');
+var _s = require('underscore.string');
 var restify = require('restify');
 var google = require('googleapis');
 var os = require('os');
@@ -30,7 +31,7 @@ var scopes = [
 ];
 
 
-function start(){
+function start() {
     if (!config.auth_code) {
 
         var server = restify.createServer({
@@ -44,6 +45,7 @@ function start(){
             startWatching();
             res.send("Authenticated :)");
             next();
+            server.stop();
         });
         server.listen(port);
         var url = oauth2Client.generateAuthUrl({
@@ -54,8 +56,7 @@ function start(){
         var command;
         if (os.platform() === 'linux') {
             command = 'xdg-open';
-        }
-        else if (os.platform() === 'darwin') {
+        } else if (os.platform() === 'darwin') {
             command = 'open';
         }
 
@@ -93,6 +94,18 @@ function startWatching() {
 }
 
 
+function getHeader(msg, header) {
+    var headers = msg.payload.headers;
+    for (var i = 0; i < headers.length; i++) {
+        if (headers[i].name === header) {
+            var name = headers[i].value.split(" ");
+            name = name.splice(0, name.length - 1).join(" ");
+            return name;
+        }
+    }
+    return null;
+}
+
 function checkMail() {
     gmail.users.messages.list({
         userId: "me",
@@ -106,7 +119,7 @@ function checkMail() {
         var messages = response.messages;
         var unseen = false;
         //unseenMessages are ordered from oldest to newest
-        for (var i = messages.length -1; i>=0 ; i--) {
+        for (var i = messages.length - 1; i >= 0; i--) {
             if (!messageHash.hasOwnProperty(messages[i].id)) {
                 unseen = true;
                 unseenMessages.push(messages[i]);
@@ -114,51 +127,19 @@ function checkMail() {
             }
         }
         if (unseen) {
-            getMessage(unseenMessages[unseenMessages.length - 1].id, function(err, message){
-                if(err){
+            //Retrieves only text mimeType for body
+            getMessage(unseenMessages[unseenMessages.length - 1].id, function(err, message) {
+                if (err) {
                     xal.error(err);
                     return;
                 }
-                xal.log.info(message);
-                var getName = function(msg){
-                    var headers = msg.payload.headers;
-                    for(var i =0;i <headers.length;i++)  {
-                        if(headers[i].name === 'From' ){
-                            var name = headers[i].value.split(" ");
-                            name = name.splice(0, name.length -1).join(" ");
-                            return name;
-                        }
-                    }
-                    return null;
-                };
-
-                var getBody = function(response){
-                    if (response.payload.parts) {
-                        var parts = response.payload.parts;
-                        xal.log.info({response: response});
-                        var body = [];
-                        for (var i = 0; i < parts.length; i++) {
-                            xal.log.info("part ", i + 1);
-                            xal.log.info(atob(parts[i].body.data));
-                            body.push(atob(parts[i].body.data));
-                        }
-                        return body;
-                    } else if (response.payload.body) {
-                        return atob(response.payload.body.data);
-                    } else {
-                        return null;
-                    }
-                };
-
-                messageHash[message.id].name = getName(message);
-                messageHash[message.id].body = getBody(message);
-                messageHash[message.id].message = message;
                 xal.log.info('You have new messages');
                 xal.createEvent('xi.event.output.text', function(state, done) {
-                    xal.log.info({state: state}, 'created event');
+                    xal.log.info({
+                        state: state
+                    }, 'created event');
 
-                    state.put('xi.event.output.text', 'You have new mail from ' + messageHash[message.id].name);
-
+                    state.put('xi.event.output.text', 'You have new mail from ' + message.name + ' with subject ' + message.subject);
                     done(state);
                 });
 
@@ -167,11 +148,13 @@ function checkMail() {
     });
 }
 
+//Retrieves message
+//stores it in messageHash and adds name, subject and body to it
 function getMessage(messageId, cb) {
     gmail.users.messages.get({
         userId: "me",
         id: messageId
-    }, function(err, response) {
+    }, function(err, message) {
         if (err) {
             xal.log.info(err);
             if (cb) {
@@ -179,14 +162,56 @@ function getMessage(messageId, cb) {
             }
             return;
         }
+
+        if (err) {
+            xal.error(err);
+            return;
+        }
+        xal.log.info(message);
+        var getName = function(msg) {
+            return getHeader(msg, "From");
+        };
+
+        var getSubject = function(msg) {
+            return getHeader(msg, "Subject");
+        };
+
+        var getBody = function(response) {
+            if (_s.startsWith(response.payload.mimeType, 'multipart')) {
+                var parts = response.payload.parts;
+                xal.log.info({
+                    response: response
+                });
+                var body = [];
+                for (var i = 0; i < parts.length; i++) {
+                    if (_s.startsWith(parts[i].mimeType, 'text')) {
+                        xal.log.info("part ", i + 1);
+                        xal.log.info(atob(parts[i].body.data));
+                        body.push(atob(parts[i].body.data));
+                    }
+                }
+                return body;
+            } else if (_s.startsWith(response.payload.mimeType, 'text')) {
+                return atob(response.payload.body.data);
+            } else {
+                return null;
+            }
+        };
+
+        messageHash[message.id].name = getName(message);
+        messageHash[message.id].body = getBody(message);
+        messageHash[message.id].subject = getSubject(message);
+        messageHash[message.id].message = message;
         if (cb) {
-            cb(null, response);
+            cb(null, messageHash[message.id]);
         }
     });
 
 }
 
 
-xal.start({name: "Gmail"}, function(){
+xal.start({
+    name: "Gmail"
+}, function() {
     start();
 });
