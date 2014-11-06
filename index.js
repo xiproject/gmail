00faca8d0d5158fbd469697c7e8f0adc2e6dc52f
Mihir Rege <mihirrege@gmail.com>
2014-11-07 05:29:40 +0530

    Add subject conversation

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var xal = require('../../xal-javascript');
 var _s = require('underscore.string');
+var _ = require('underscore');
 var atob = require('atob');
 var gmail = require('./gmail');
 var messageHash = {};
 var unseenMessages = [];
-
+var inputManager;
 function getHeader(msg, header) {
     var headers = msg.payload.headers;
     for (var i = 0; i < headers.length; i++) {
@@ -43,6 +44,8 @@ function checkMail() {
                     xal.log.error(err);
                     return;
                 }
+                xal.log.info(message);
+                if(_.contains(message.labelIds, "CATEGORY_FORUMS")){
                     return;
                 }
                 xal.log.info('You have new messages');
@@ -51,7 +54,7 @@ function checkMail() {
                         state: state
                     }, 'created event');
 
-                    state.put('xi.event.output.text', 'You have new mail from ' + message.name + ' with subject ' + message.subject);
+                    state.put('xi.event.output.text', 'You have new mail from ' + message.name );
                     done(state);
                 });
 
@@ -120,11 +123,39 @@ function getMessage(messageId, cb) {
 
 }
 
+//TODO: assuming stuff
+xal.on('xi.event.input.destination', function(state, next){
+    var dest = _.reduce(state.get('xi.event.input.destination'), function(memo, dest) {
+        if (dest.source === inputManager.id) {
+            memo = dest;
+        }
+        return memo;
+    }, null);
+    if (dest && dest.value == xal.getId()) {
+        xal.log.info({dest: dest}, 'Acting on InputManager\'s command');
+        var text = state.get('xi.event.input.text')[0].value;
+        if(unseenMessages[unseenMessages.length - 1].subject){
+            state.put('xi.event.input.destination', xal.getId());
+            state.put('xi.event.output.text', 'The subject is ' + unseenMessages[unseenMessages.length - 1].subject);
+        }
+    }
+    next(state);
+
+});
 
 xal.start({
     name: "Gmail"
 }, function() {
     gmail.start(function(){
         setInterval(checkMail, 10000);
+    });
+    xal.getAgent({name: 'InputManager'}, function(err, agent) {
+        if (err) {
+            xal.log.fatal(err);
+        } else if (agent === null) {
+            xal.log.fatal('InputManager is required');
+        } else {
+            inputManager = agent;
+        }
     });
 });
